9e7724164cddf547f2b00b600e640f680aef3182
molasun <mola79321@gmail.com>
2022-04-11 12:48:27 +0800

    Add source code

---
+++ quarkus-workshop-m1m2-labs/src/main/java/org/acme/people/rest/GreetingResource.java
@@ -0,0 +1,60 @@
+package org.acme.people.rest;
+
+import java.util.Optional;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.acme.people.service.GreetingService;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.smallrye.common.annotation.NonBlocking;
+
+@Path("/hello")
+public class GreetingResource {
+
+    public static final Logger log = LoggerFactory.getLogger(GreetingResource.class);
+
+    @Inject
+    GreetingService service;
+
+    @ConfigProperty(name = "greeting.message")
+    String message;
+
+    @ConfigProperty(name = "greeting.suffix", defaultValue = "!")
+    String suffix;
+
+    @ConfigProperty(name = "greeting.name")
+    Optional<String> name;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    @NonBlocking
+    public String greeting(@PathParam("name") String name) {
+        return service.greeting(name);
+    }
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @NonBlocking
+    public String hello() {
+        return message + " " + name.orElse("world") + suffix;
+    }
+
+    @GET
+    @Path("/lastletter/{name}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String lastLetter(@PathParam("name") String name) {
+        int len = name.length();
+        String lastLetter = name.substring(len - 1);
+        log.info("Got last letter: " + lastLetter);
+        return lastLetter;
+    }
+}
---
+++ quarkus-workshop-m1m2-labs/src/test/java/org/acme/people/GreetingResourceTest.java
@@ -0,0 +1,36 @@
+package org.acme.people;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class GreetingResourceTest {
+
+    @Test
+    public void testHelloEndpoint() {
+        given()
+                .when().get("/hello")
+                .then()
+                .statusCode(200)
+                .body(is("hi quarkus!"));
+    }
+
+    // add more tests
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+                .pathParam("name", uuid)
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(startsWith("hello " + uuid));
+    }
+}
